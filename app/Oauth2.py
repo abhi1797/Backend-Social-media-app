a4e89212128efc9324c7c93ada463792bc22ba81
abhi1797 <abhijeet.sarkar112@gmail.com>
2025-01-29 15:31:42 +0530

    Oauth2 file configuration

--- app/Oauth2.py
+++ app/Oauth2.py
@@ -4,7 +4,7 @@ from . import schemas,database, models
 from fastapi import status, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-
+from .config import settings
 
 #this give the token url when user tries to login with its username and password
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
@@ -14,14 +14,14 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 #openssl rand -hex 32
 
-SECRET_KEY = "1b0b01deb22e015c2e69937713016261699ae7c5fd9fe63ae5deaf9f59ea6e97"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_TIME = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_TIME)
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expire})
 
